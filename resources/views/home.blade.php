d7e57b6f54775bcbde2b31c9cd76bf68b1dd1c4c
felipesantos22 <felipevs2018@gmail.com>
2024-05-19 01:06:40 -0300

    change login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('layout')
+
     <h1>Home</h1>
     {{$name}}
-</body>
-</html>
+    {{$test ?? 'Variavel teste não existe'}}
+    {{isset($record)? 'Existe' : 'Não existe'}}
+
+    
+@endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+{{-- Nome da pasta que vai herdar as formatações --}}
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
-    <title>Document</title>
-</head>
+{{-- Nome que está no yield --}}
+@section('layout')
 
 <body>
     <div class="login-container">
@@ -22,5 +17,5 @@
         </form>
     </div>
 </body>
-
-</html>
+    
+@endsection
