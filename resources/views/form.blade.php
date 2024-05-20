20d0e06093b11068506d761e77b63b5337d02cb0
felipesantos22 <felipevs2018@gmail.com>
2024-05-20 20:57:54 -0300

    change list

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,18 +1,26 @@
-<div class="container">
-    <form action="{{ route('tasks.create') }}" method="POST">
-        @csrf
-        <label for="name">Tarefa:</label>
-        <input type="text" id="name" name="name" required>
-        <button type="submit">Adicionar Tarefa</button>
-    </form>
-</div>
+@extends('pages.layout')
 
-@if ($tasks->isEmpty())
-<p>Nenhuma tarefa encontrada.</p>
-@else
-<ul>
-    @foreach ($tasks as $task)
-        <li>{{ $task->name }}</li>
-    @endforeach
-</ul>
-@endif
+@section('app')
+    <div class="to-do">
+        <form id="task-form" action="{{ route('tasks.create') }}" method="POST">
+            @csrf
+            <div class="insert">
+                <input class="textInsert" type="text" id="name" name="name" required />
+                <button type="submit"><i class="bi bi-plus-circle" style="font-size: 30px;"></i></button>
+            </div>
+            <ul class="task-list">
+                @forelse ($tasks as $task)
+                    <li class="task-item">
+                        {{ $task->name }}
+                        <div class="icons">
+                            <i class="bi bi-trash3-fill"></i>
+                            <i class="bi bi-pencil-square"></i>
+                        </div>
+                    </li>
+                @empty
+                    <li class="no-tasks">Nenhuma tarefa encontrada.</li>
+                @endforelse
+            </ul>
+        </form>
+    </div>
+@endsection
